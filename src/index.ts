516cd3dee4a0973fd396fe119ffbbdfd9db42456
hd49web <hd49web@gmail.com>
2022-08-06 22:42:01 -0300

    entregar

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+let saldoTotal = 0
+limparSaldo()
+function somarAoSaldo(soma: number) {
+    if (campoSaldo) {
+        saldoTotal += soma
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+function limparCampoSoma() {
+    soma.value = "";
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value)); 
+    });
+}
+botaoLimpar.addEventListener('click', () => { // Percebam que aqui o typescript não acusou o botao de ser nulo e não precisei do if. Caso queiram fazer o teste, retirem a exclamação.
+    limparSaldo();
+});
+
+
+
+
